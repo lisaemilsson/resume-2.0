ec2d7f87c99467c9ee6472a1c08da4b1944271af
lisaemilsson <114024175+lisaemilsson@users.noreply.github.com>
2022-11-10 21:57:42 +0100

    fixed spinner

--- javascript/api.js
+++ javascript/api.js
@@ -1,26 +1,36 @@
 let portfolioUl = document.getElementById("image-gallery");
 
+const loadingDiv = document.getElementById('loading');
+
+function showSpinner() {
+loadingDiv.style.visibility = 'visible';
+}
+
+function hideSpinner() {
+loadingDiv.style.visibility = 'hidden';
+}
+
 async function getRepos() {
     const url = "https://api.github.com/users/lisaemilsson/repos";
+
+    showSpinner()
+    await new Promise(r => setTimeout(r, 2000));
 
     let repos = await fetch(url)
         .then(response => response.json());
 
-        // console.log(repos)
-
         repos.forEach(repo => {
-            // console.log(repo)
 
             const li = document.createElement("li")
             const img = document.createElement("img")
 
-            if(repo.name === 'js--number-guessing-game'){
+            if(repo.name === 'js-number-guessing-game'){
                 img.src = "../img/guessing.JPG"
             }
             if(repo.name === 'resume-page'){
                 img.src = "../img/img_resume..png";
             }
-            if(repo.name === 'js--sicssor-papper-rock'){
+            if(repo.name === 'js-sicssor-papper-rock'){
                 img.src = "../img/sicssor_papper_rock.JPG";
             }
             if(repo.name === 'resume-2.0'){
@@ -48,17 +58,9 @@ async function getRepos() {
             div.appendChild(a);
 
             portfolioUl.appendChild(li)
+            hideSpinner()
         })
 
-        const loadingDiv = document.getElementById('image-gallery');
-
-            function showSpinner() {
-            loadingDiv.style.visibility = 'visible';
-            }
-
-            function hideSpinner() {
-            loadingDiv.style.visibility = 'hidden';
-            }
 }
 
 getRepos()
--- javascript/spinner.js
+++
@@ -1,9 +0,0 @@
-const loadingDiv = document.getElementById('img-gallery');
-
-function showSpinner() {
-  loadingDiv.style.visibility = 'visible';
-}
-
-function hideSpinner() {
-  loadingDiv.style.visibility = 'hidden';
-}
